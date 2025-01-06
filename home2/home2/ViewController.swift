78fcbdd167d673a7e753c30f72fa6cdcb7dfb3fc
ilya.devel <ilya.devel@yandex.ru>
2025-01-06 11:52:28 +0500

    init home2

---
+++ home2/home2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  home2
+//
+//  Created by Ильяс Альфатович on 06.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
